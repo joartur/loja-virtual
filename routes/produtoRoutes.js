94eb645e016b9c9ef143844c23d22a4222bb7635
Joamerson Artunes <145156905+joartur@users.noreply.github.com>
2024-09-08 18:52:35 -0300

    Delete routes directory

--- routes/produtoRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ProdutoController = require('../Controllers/ProdutoController');
-
-router.get('/', ProdutoController.showProdutos);
-
-module.exports = router;
